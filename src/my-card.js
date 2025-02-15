9050c4884fbfcaea363cd8942c5509c2a583f7e3
GavinMalzahn <gavin.malzahn@gmail.com>
2025-02-15 17:26:47 -0500

    done with card

--- src/my-card.js
+++ src/my-card.js
@@ -17,79 +17,121 @@ export class MyCard extends LitElement {
     this.title = "Plo Koon";
     this.image = "https://cdn.shopify.com/s/files/1/0883/1929/5779/files/image-8.png";
     this.link = "https://hax.psu.edu/";
+    this.fancy = false;
+    this.description = "hi";
   }
 
   static get styles() {
     return css`
       :host {
-        display: block;
+        display: inline-block;
       }
-      #card-list {
-        display: flex;
+      :host([fancy]) .card{
+        background-color: blue;
+        color: white;
+      }
+      .cardList{
+        display: inline-flex;
       }
       .card {
         background-color: #400000;
         color: grey;
-        max-width: 400px;
-        border-width: 4;
-        border-radius: 8%;
-        padding: 8px 8px 8px 32px;
-        margin: 8px;
+        width: 366px;
+        border-width: 16px;
+        border-radius: 6%;
+        padding: 16px;
+        margin: 16px;
+      }
+      .card-title{
+        padding: 0px 8px;
+        height: 30px;
+        overflow: hidden;
       }
       img{
-        width: 375px;
+        width: 350px;
+        padding: 8px;
+        aspect-ratio: 1/1;
+        
+      }
+      details summary {
+        text-align: left;
+        font-size: 20px;
+        padding: 8px 0;
+      }
+
+      details[open] summary {
+        font-weight: bold;
+      }
+      details div {
+        border: 2px solid black;
+        text-align: left;
+        padding: 8px;
+        height: 70px;
+        overflow: auto;
       }
 
       .card:hover {
         background-color: blue;
         border: 1px solid black;
       }
-
-      @media not screen and (min-width: 500px) and (max-width: 800px) {
-        .details{
-          display: none;
-        }
-      }
-      @media only screen and (max-width: 500px) {
-        div{
-          width: 300px;
-        }
-        img{
-          width: 275px;
-        }      
+     
       }`;
   }
+  // put this anywhere on the MyCard class; just above render() is probably good
+  openChanged(e) {
+    console.log(e.newState);
+    if (e.newState === "open") {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
+  }
+  
 
   render() {
     return html`
-    <div>
-    <button class="duplicate">Clone Card</button>
-    <button id="namechange">Change name</button>
-    <button id="pic-change">Change pic.</button>
-    <button id="bg-change">Change background</button>
-    <button id="delete">Delete card</button>
-    </div>
+    <button @click="${() => { this.fancy = !this.fancy }}">Toggle</button>
     <div id="cardList">
       <div class="card">
       <h2 class="card-title">
       ${this.title}
     </h2>
     <img src=${this.image} alt="Plo Koon" class="card-image">
-    
-  <p>  
-  Home Planet: Dorin
-  <br>Species: Kel Dor
-  <br>Height: 1.88 meters/6ft 2inch
-  <br>Mass: 80 kilograms
-  <br>Padawan: Bultar Swan
-    <br><a class="details" href=${this.link}>Details</a>
-    </p> 
+    <table>
+        <td>Home Planet: </td>
+        <td><slot name="planet">Dorin</slot></td>
+      </table>
+      <table>
+        <td>Species: </td>
+         <td><slot name="species">Kel Dor</slot></td>
+      </table>
+  
+  <!-- put this in your render method where you had details -->
+  <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+    <summary>Description</summary>
+    <div>
+      <table>
+        <td>Height: </td>
+        <td><slot name="height">1.88 meters/6ft 2inch</slot></td>
+      </table>
+      <table>
+        <td>Mass: </td>
+        <td><slot name="mass">80 kilograms</slot></td>
+      </table>  
+      <table>
+        <td>Padawan(s): </td>
+        <td><slot name="padawan">Bultar Swan</slot></td>
+      </table>
+    </div>
+  </details>
       </div>
       </div>`;
   }
 
   static get properties() {
     return {
+      fancy:{ type:Boolean, reflect: true },
       title: { type: String },
       image: { type: String },
       link: { type: String },
